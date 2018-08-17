148c531b4fcfac0d6d0642df0cc2416a94bb62a4
martin <ktmjmartin@gmail.com>
2018-08-17 22:05:05 +0200

    fin ancien projet java

--- src/DAO/dao.java
+++ src/DAO/dao.java
@@ -17,6 +17,11 @@ public abstract class dao<T> {
 	public abstract T find(int id);
 
 	public abstract T connecter(String string, String string2);
-	public abstract T EstDejaChauffeur(int id_pers1,int id_balade1);
+	public abstract T EstDejaChauffeur(int id_pers1,int id_balade1); //doit etre dans une classe appelle une liste 
 	public abstract T baladeCategorie(int id_balade);
 }
+// On ne doit pas avoir la classe passage ni CatPersonne
+// eviter de voir 
+//revoir les calculs pour une balade    forfait 
+//revoir tout avec les membres 
+//decrementation losque l'on fait une reservation 
